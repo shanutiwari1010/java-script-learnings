e6a2dcca4625bff748ef2c24d30afeb711cb96c7
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-01-13 17:31:27 +0530

    added project01

---
+++ project01/script.js
@@ -0,0 +1,15 @@
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+let count = 1;
+setInterval( ()=>{
+    if(count < 1000){
+        count++;
+        counter.innerText = count;
+    }
+
+} ,10)
+
+setTimeout(()=>{
+    followers.innerText= "followers on instagram!"
+},3000)
