b1142d03c4a102a90aaafc732a28b64b6bfd4966
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-25 23:20:01 +0530

    practicing dom

---
+++ 10practice/app.js
@@ -0,0 +1,9 @@
+// const smallImg = document.getElementsByClassName("oldImg")
+
+// for(let i =0 ; i<smallImg.length ; i++){
+//    smallImg[i].src = "assets/spiderman_img.png"
+// }
+
+console.log(document.querySelector("h1"))
+console.log(document.querySelector("#description"))
+
